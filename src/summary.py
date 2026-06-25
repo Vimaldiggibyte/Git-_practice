bb43116a8b21a3e74482c9178f9b66d7004c99c7
VimalRajSt <vimalrajtemp@gmail.com>
2026-06-25 13:07:06 +0530

    Column one print
    Column 2 print
    
    Column 3 print

--- src/summary.py
+++ src/summary.py
@@ -5,5 +5,10 @@ df = pd.read_csv(r"C:\practice_git\data\clean_sales.csv")
 
 print(df.describe())
 
-#print(' Columns : ',df.columns)
-#print(' Rows : ',df.shape)
+print(' Columns : ',df.columns)
+print(' Rows : ',df.shape)
+print("Print only the column one ", df.columns[0])
+print("Print only the column two ", df.columns[1])
+print("Print only the column three ", df.columns[2])
+
+
